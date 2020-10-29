70e3c97132da5f37bad00cd2d50a09319ab4b833
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2020-10-29 13:03:40 +0100

    deleted the 'decorators' script

--- app/models.py
+++ app/models.py
@@ -4,5 +4,5 @@ class DailymailColumn(db.Model):
 	__tablename__ = 'dailymail_columns'
 	id = db.Column(db.Integer, primary_key=True)
 	link = db.Column(db.String, nullable=False, unique=True)
-	title = db.Column(db.String, nullable=False, unique=True)
-	columnist = db.Column(db.String, nullable=False, unique=True)
+	title = db.Column(db.String, nullable=False)
+	columnist = db.Column(db.String, nullable=False)
