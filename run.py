081b108f2a5ef027904f7c77507fba81ecc0536f
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2020-11-01 10:58:40 +0100

    added a minor change to know if the app is scraping on heroku

--- run.py
+++ run.py
@@ -16,17 +16,6 @@ def make_shell_context():
     return dict(db=database, DailymailColumn=DailymailColumn)
 
 @app.cli.command()
-@click.argument('test_names', nargs=-1)
-def test(test_names):
-    """Run the unit tests."""
-    import unittest
-    if test_names:
-        tests = unittest.TestLoader().loadTestsFromNames(test_names)
-    else:
-        tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-@app.cli.command()
 def deploy():
     """
     Run deployment tasks.
