64302bc66dd6c26782748a4d4691e501028d267c
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2020-12-26 19:54:40 +0100

    added pagination

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .models import DailymailColumn
 
 blue_print = Blueprint('views', __name__)
 
 @blue_print.route('/')
 def main():
-	columns = DailymailColumn.query.all()
-	return render_template('index.html', columns=columns)
+	page = request.args.get('page', 1, type=int)
+	pagination = DailymailColumn.query.paginate(page, per_page=10, error_out=False)
+	columns = pagination.items
+	return render_template('index.html', columns=columns, pagination=pagination)
