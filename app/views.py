0356aa5f41db7a1bc1ef0d0da5f7ea4f6ce60704
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2020-12-27 10:02:35 +0100

    made a few very minor modifications after the initial migration

--- app/views.py
+++ app/views.py
@@ -6,6 +6,6 @@ blue_print = Blueprint('views', __name__)
 @blue_print.route('/')
 def main():
 	page = request.args.get('page', 1, type=int)
-	pagination = DailymailColumn.query.paginate(page, per_page=10, error_out=False)
+	pagination = DailymailColumn.query.order_by(DailymailColumn.date_added.desc()).paginate(page, per_page=10, error_out=False)
 	columns = pagination.items
 	return render_template('index.html', columns=columns, pagination=pagination)
