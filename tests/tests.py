8e83d22d45dff73431893cad4bd21e82fbe76db4
pulga <akindeleodedoyin@gmail.com>
2021-01-30 18:59:32 +0100

    added tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,8 @@
 import unittest
-from flask import current_app
+from flask import current_app, url_for
 from app import create_app, database
-
+from app.models import DailymailColumn
+from datetime import datetime
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
@@ -10,6 +11,11 @@ class BasicsTestCase(unittest.TestCase):
         self.app_context.push()
         database.create_all()
         self.client = self.app.test_client(use_cookies=True)
+        self.column = DailymailColumn(link='https://www.test.com',
+            title='test title',
+            columnist='test columnist')
+        database.session.add(self.column)
+        database.session.commit()
 
     def tearDown(self):
         database.session.remove()
@@ -21,3 +27,14 @@ class BasicsTestCase(unittest.TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+    def test_column_content(self):
+        self.assertEqual(f'{self.column.link}', 'https://www.test.com')
+        self.assertEqual(f'{self.column.title}', 'test title')
+        self.assertEqual(f'{self.column.columnist}', 'test columnist')
+        self.assertTrue(isinstance(self.column.date_added, datetime))
+
+    def test_columns_list_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('test title' in response.get_data(as_text=True))
